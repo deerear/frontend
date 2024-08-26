7892bf3b84d9567027db772e32370eb9c7313ee8
garden11 <emailuseuse@gmail.com>
2024-08-27 01:34:45 +0900

    feat: initialize storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,15 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react';
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/i,
-      },
+        date: /Date$/i
+      }
     },
-  },
+    layout: 'centered'
+  }
 };
 
 export default preview;
