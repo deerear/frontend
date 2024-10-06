f0bdb3303ebd1eaaadf8cfda5194fb6ec5b30376
garden11 <emailuseuse@gmail.com>
2024-10-07 00:20:24 +0900

    feature: update global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,7 +16,9 @@ const queryClient = new QueryClient({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <div className='container'>
+        <Component {...pageProps} />
+      </div>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
